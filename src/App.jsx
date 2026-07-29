2a758080af991f70abfd51ae6cb9985e2ce2a088
Satyaa1997 <satyamrockindia@gmail.com>
2026-07-29 12:13:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import MobileHome from "./Components/MobileHome";
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 const App = () => {
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 992);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 992);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div>
       <Navbar />
-
       <Routes>
-       
-          <Route path="/" element={isDesktop ? <Home /> : <MobileHome />} />
-
+         <Route path="/" element={<Home/>} />
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
